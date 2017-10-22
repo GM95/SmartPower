4e5a9d2a229d02c98ed9f07e79a50401e3e97468
GM95 <gmarkiewicz2@gmail.com>
2017-10-22 23:06:37 +0200

    Sprzatanie kodu :)

--- data_sym.py
+++ data_sym.py
@@ -3,51 +3,44 @@ import MySQLdb
 import datetime
 from threading import Thread
 
+class GeneratingRandomData(Thread):
 
-class generowanieDanychDoBazy(Thread):
-
-
-    connection = None
     cursor = None
-    thread1 = None
-    thread2 = None
+    connection = None
 
-    def run(self):
-        thread1 = Thread(target=self.polaczSieZBazaDanych())
-        thread2 = Thread(target=self.zakonczDzialanieProgramu())
+    def run(self):                                              #funkcja wykonywana automatycznie po stworzeniu obiektu
+        thread1 = Thread(target=self.connect_to_database())
+        thread2 = Thread(target=self.close_program())
         return thread1, thread2
 
-    def zakonczDzialanieProgramu(self):
+    def close_program(self):
         pass
 
+    def connect_to_database(self):
+        self.connection = MySQLdb.connect(user="root", passwd="lenovo", db="bolid") #laczenie z baza
+        self.cursor = self.connection.cursor()
+        self.cursor.execute("SELECT MAX(id) FROM data")
+        max_id = self.cursor.fetchone() #maxId to maksymalne id w bazie
+        max_id = str(max_id)
+        max_id = max_id[1:-2]
+        print(max_id)
+        return max_id, self.cursor
 
-    def polaczSieZBazaDanych(self):
-        connection = MySQLdb.connect(user="root", passwd="lenovo", db="bolid") #laczenie z baza
-        cursor = connection.cursor()
-        cursor.execute("SELECT MAX(id) FROM data")
-        maxId = cursor.fetchone() #maxId to maksymalne id w bazie
-        maxId = str(maxId)
-        maxId = maxId[1:-2]
-        print(maxId)
-        return maxId, cursor
-
-
-    def dodawajWpisyDoBazyDanych(self):
+    def add_data_to_base(self):
         while 1:
             try:
-                maxId = int(maxId)
-                maxId = maxId+1
-                maxId = str(maxId)
-                dodajWpisDoBazy = ("INSERT INTO data (id,czas,napiecie,prad,predkosc) VALUES (%s,%s,%s,%s,%s)")
-                daneDoDodania = (maxId,datetime.datetime.now(),"23.2","24.2","25.2")
-                cursor.execute(dodajWpisDoBazy,daneDoDodania)
-                connection.commit()
+                max_id = int(max_id)
+                max_id = max_id+1
+                max_id = str(max_id)
+                add_line_to_base = ("INSERT INTO data (id,czas,napiecie,prad,predkosc) VALUES (%s,%s,%s,%s,%s)")
+                data_to_add = (max_id,datetime.datetime.now(),"23.2","24.2","25.2") #dorobić generator liczb losowych :)
+                self.cursor.execute(add_line_to_base, data_to_add)
+                self.connection.commit()
                 time.sleep(2)
-                print(maxId)
+                print(max_id)
             except:
                 print("blad przy probie zapisu danych")
                 break
 
-
-    def wyczyscBazeDanych(self):
-        cursor.execute("DELETE FROM data WHERE id>1")
+    def clear_database(self):
+        self.cursor.execute("DELETE FROM data WHERE id>1")
--- fetch_data.py
+++ fetch_data.py
@@ -2,24 +2,38 @@ import datetime
 import MySQLdb
 
 
-global zapytanie
-global id
-global napiecie
-global prad
-global predkosc
+class GetDataFromDataBase():
+
+    def __init__(self):
+        self.connection = MySQLdb.connect(user="root", passwd="lenovo", db="bolid")
+
+    def get_last_row(self):
+        pass
+
+    def get_current(self):
+        pass
+
+    def get_voltage(self):
+        pass
+
+    def get_speed(self):
+        pass
 
 
-def pobiezczas():
+czas = None
+
+
+def get_time():
     global czas
     czas = datetime.datetime.now()
     return czas
 
 
-cnx = MySQLdb.connect(user="root",passwd="lenovo",db="bolid")
+cnx = MySQLdb.connect(user="root", passwd="lenovo", db="bolid")
 print(str(cnx))
 cur = cnx.cursor()
 dane = cur.execute("SELECT prad FROM data")
 i = cur.fetchall()
 print(str(i))
-pobiezczas()
+get_time()
 print(str(czas))
