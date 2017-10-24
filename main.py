efd0895fe2fb7a59b7c4b565326e5f8c79abe318
GM95 <gmarkiewicz2@gmail.com>
2017-10-24 20:46:14 +0200

    Uruchamia program

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 import tkinter_widgets
-import tkinter as tk
 
-
-root = tk.Tk()
-app = tkinter_widgets.FullScreenAppMainWindow(root)
-root.mainloop()
+app = tkinter_widgets.FullScreenAppMainWindow()
+app.mainloop()
